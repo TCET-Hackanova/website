fcc7933b13f74fd289ba9dfa757da55a71bfe7ea
jwalapc <rohitchaurasiya153@gmail.com>
2023-11-18 21:08:35 +0530

    fixed

--- js/faq.js
+++ js/faq.js
@@ -2,53 +2,7 @@ const accordionBtns = document.querySelectorAll('.accordion__title');
 const divCollapse = document.querySelectorAll('.accordion__collapse');
 
 // accordionBtns.forEach((button) => {
-// 	button.addEventListener('click', (event) => {
-// 		// console.log('click')
 
-// 		let accCollapse = button.nextElementSibling;
-
-// 		if (!button.classList.contains('collapsing')) {
-// 			// open accordion item
-// 			if (!button.classList.contains('open')) {
-// 				// console.log("click");
-
-// 				accCollapse.style.display = 'block';
-// 				let accHeight = accCollapse.clientHeight;
-// 				// console.log(accHeight)
-
-// 				setTimeout(() => {
-// 					accCollapse.style.height = accHeight + 'px';
-// 					accCollapse.style.display = '';
-// 				}, 1);
-
-// 				accCollapse.classList.add('collapsing');
-
-// 				setTimeout(() => {
-// 					//   console.log("click");
-// 					// accCollapse.classList = 'accordion__collapse collapse open';
-// 					accCollapse.classList.add('open');
-// 				}, 300);
-// 			}
-// 			//close accordion item
-// 			else {
-// 				// accCollapse.classList = 'accordion__collapse collapsing';
-// 				accCollapse.classList.remove('open');
-// 				setTimeout(() => {
-// 					accCollapse.style.height = '0px';
-// 				}, 1);
-
-// 				//
-// 				setTimeout(() => {
-// 					//   console.log("click2");
-// 					accCollapse.classList = 'accordion__collapse collapse';
-// 					accCollapse.style.height = '';
-// 				}, 300);
-// 			}
-
-// 			button.classList.toggle('open');
-// 		}
-// 	});
-// });
 
 // document.addEventListener('DOMContentLoaded', function (event) {
 var acc = document.getElementsByClassName('accordion__title');
